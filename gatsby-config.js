76ac9eaf8b97eedfdafb3d3601a9552a946232dd
koji <kk2796@nyu.edu>
2020-01-03 18:45:08 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: "koji kanao",
+    title: "Koji Kanao",
     author: "Koji Kanao",
     github: "https://github.com/koji",
     instagram: "https://www.instagram.com/koji_kanao/",
