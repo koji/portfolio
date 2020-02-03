d270be15f8c4a37747065495fbbbf9a5deb23026
koji <kk2796@nyu.edu>
2020-02-03 10:35:31 -0500

    remove grid component from footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { useSiteMetadata } from "../hooks/siteMetadata";
 import styled from "styled-components";
-import Grid from "styled-components-grid";
 
 const FooterWrapper = styled.footer`
   background: #111111;
@@ -11,33 +10,24 @@ const FooterWrapper = styled.footer`
   margin-top: 3rem;
 `;
 
-const Text = styled.p`
-  margin: 10px 0;
-`;
-
 const GridWrapper = styled.div`
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-gap: 1em;
-  grid-auto-rows: minmax(100px, auto);
+  grid-template-columns: repeat(4, 1fr);
+  /* grid-gap: 1em; */
+  grid-auto-rows: minmax(200px, auto);
 `;
 
-const Cell = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  /* grid-gap: 1em; */
-  /* background: #eee; */
-  /* text-align: center; */
-  /* grid-auto-rows: minmax(100px, auto); */
-  grid-auto-columns: minmax(300px, auto);
+const FooterGrid = styled.div`
+  grid-column: 1/2;
+  /* min-height: 12em; */
 `;
 
 export const Footer: React.FC = () => {
   const { author } = useSiteMetadata();
   return (
     <FooterWrapper>
-      <Grid valign="center" style={{ minHeight: "12em" }}>
-        <Grid.Unit size={0.25}>
+      <GridWrapper>
+        <FooterGrid>
           <p style={{ textAlign: "left", marginLeft: "20px" }}>
             🤘 Hi, I'm Koji, a software engineer
             <br />I like to do creative coding and
@@ -46,9 +36,8 @@ export const Footer: React.FC = () => {
             <br />
             I'm in NYC!
           </p>
-        </Grid.Unit>
-        <Grid.Unit size={0.75}></Grid.Unit>
-      </Grid>
+        </FooterGrid>
+      </GridWrapper>
     </FooterWrapper>
   );
 };
