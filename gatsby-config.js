aacb74064a2525900149661ffc863039113d7d46
koji <kk2796@nyu.edu>
2020-06-05 23:29:15 -0400

    update site info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `koji portfolio `,
+    description: `koji kanao portfolio skills and projects`,
+    author: `koji kanao`,
   },
   plugins: [
     `gatsby-plugin-styled-components`,
