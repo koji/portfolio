499d564304df35887ae3c41b6e732e9882d42ada
koji <kk2796@nyu.edu>
2020-06-13 02:35:51 -0400

    Update appbar.tsx

--- src/components/appbar.tsx
+++ src/components/appbar.tsx
@@ -2,19 +2,21 @@ import React from 'react';
 import Intent from './link';
 
 const Appbar = () => (
-  <div className="container my-3">
-    <ul className="nav justify-content-center">
-      <li className="nav-item mx-3">
-        <Intent to="/">Home</Intent>
+  <div className='container my-3'>
+    <ul className='nav justify-content-center'>
+      <li className='nav-item mx-3'>
+        <Intent to='/'>Home</Intent>
       </li>
-      <li className="nav-item mx-3">
-        <Intent to="/projects">Projects</Intent>
+      <li className='nav-item mx-3'>
+        <Intent to='/projects'>Projects</Intent>
       </li>
-      <li className="nav-item mx-3">
-        <Intent to="/oss">OSS</Intent>
+      <li className='nav-item mx-3'>
+        <Intent to='/oss'>OSS</Intent>
       </li>
-      <li className="nav-item mx-3">
-        <a className="nav-link" href="https://drive.google.com/file/d/1dAHY3C69_FFo0AJ7_jiBBYuTWUZGQ4BA/view?usp=sharing">
+      <li className='nav-item mx-3'>
+        <a
+          className='nav-link'
+          href='https://drive.google.com/file/d/1dAHY3C69_FFo0AJ7_jiBBYuTWUZGQ4BA/view?usp=sharing'
         >
           CV
         </a>
