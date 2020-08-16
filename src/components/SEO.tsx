06b372bd2d4be8256d351d1ec228be74ed66e3bc
Koji <kk2796@nyu.edu>
2020-08-16 15:03:46 -0400

    Modify SEO component

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -1,11 +1,49 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { StaticQuery, graphql } from 'gatsby';
 
-const SEO = () => {
-    return (
-        <div></div>
-    )
+type SEOData = {
+  title: string;
+  description: string;
+  keywords: string;
 }
+
+const SEO = ({ title, description, keywords }: SEOData) => (
+  <StaticQuery
+    query={query}
+    render={({
+      site: {
+        siteMetadata: { defaultTitle, defaultDescription, defaultKeywords },
+      }
+    }) => {
+      const seo = {
+        title: title ? title : defaultTitle,
+        description: description ? description : defaultDescription,
+        keywords: keywords ? keywords : defaultKeywords,
+      };
+      return (
+        <Helmet>
+          <html lang='en' />
+          <title>{seo.title}</title>
+          <meta name='description' content={seo.description} />
+          <meta name='keywords' content={seo.keywords} />
+          <meta name='robots' content='index, follow' />
+        </Helmet>
+      );
+    }}
+  />
+);
 
 export default SEO;
 
+const query = graphql`
+  {
+    site {
+      siteMetadata {
+        defautlTitle: title
+        defaultDescription: description
+        defaultKeywords: keywords
+      }
+    }
+  }
+`;
