22d2bfbf1553dda3b45d4a92fb62659a6823faab
koji <kk2796@nyu.edu>
2020-12-31 03:22:34 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,7 +69,7 @@ module.exports = {
         // The property ID; the tracking code won't be generated without it
         trackingId: `UA-103643560-1`,
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
+        head: false,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
